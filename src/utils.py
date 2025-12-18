e307a6bc20380f685414db7d91644b83849e49d7
navenavelimargam <navenavelim@gamil.com>
2025-12-18 15:16:51 +0530

    Deployment congiguration

--- src/utils.py
+++ src/utils.py
@@ -40,6 +40,9 @@ def evaluate_model(x_train, y_train, x_test, y_test, models, param):
 
     except Exception as es:
         raise costomexception(es, sys)
-
-
-    
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as es:
+        raise costomexception(es,sys)    
